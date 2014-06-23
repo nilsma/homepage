9f9fcf82d93f449fd855ed3dca92ab18b0a2405a
nilsma <nilsma231@gmail.com>
2014-06-23 19:01:40 +0200

    added html structure and basic css

---
+++ html/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style/main.css">
+    <title>Nils Martinussen</title>
+</head>
+<body>
+<div id="main_container">
+    <header>
+        <div>
+            <h1>Nils Martinussen</h1>
+            <hr id="red_line"/>
+            <h2>Web Design & Development</h2>
+        </div>
+    </header>
+    <div id="pres_main">
+        <div id="pres_inner">
+            <div>
+                <h3 id="top_header">Homepage for Bergen Rugbyklubb 2013</h3>
+                <img id="screenshot" src="images/screenshot_rugby.jpg" alt="project screenshot"/>
+            </div>
+            <div>
+                <h3 id="bottom_header">Homepage for Bergen Rugbyklubb 2013</h3>
+                <p class="text">My very first, solo commercial assignment. Basically to learn XML implementation in PHP.</p>
+                <p>Among the client's requirements were articles, match reports and training schedule on the front page without lenghty vertical scrolling, as well as a concrete identity connected to rugby.</p>
+                <p>Official homepage maintained by Bergen Rugbyklubb.</p>
+                <p class="link"><a href="http://brk.nima-design.net">Visit presentation project</a></p>
+            </div>
+            <img id="arrow_left" src="images/arrows-back-icon.png">
+            <img id="arrow_right" src="images/arrows-forward-icon.png">
+        </div>
+    </div>
+    <footer>
+        <div>
+            <h3>&copy; nils martinussen 2012</h3>
+            <h4><a class="focusTextWhite" href="mailto:nils%2emartinussen%40gmail%2ecom?subject=Web Design" tabindex="5" title="Send me an email">nils@nima-design.net</a></h4>
+        </div>
+    </footer>
+</div> <!-- end #main_container -->
+</body>
+</html>
